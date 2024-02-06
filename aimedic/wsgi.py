2bb09e9b7ffafc7502b77012cbe0a16cf1dba014
olaifa ibukun <ibukunolaifa1984@example.com>
2024-02-06 15:39:34 +0100

    production

--- aimedic/wsgi.py
+++ aimedic/wsgi.py
@@ -2,12 +2,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# from aimedic.settings import base_settings
+from aimedic.settings import base_settings
 
-# if base_settings.DEBUG:
-#     os.environ.setdefault(
-#         "DJANGO_SETTINGS_MODULE", "aimedic.settings.development_settings"
-#     )
+if base_settings.DEBUG:
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", "aimedic.settings.development_settings"
+    )
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aimedic.settings.production_settings")
 
