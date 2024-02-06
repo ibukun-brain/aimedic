bcfb9b057c05b1da5d114869ab3ae69964728d7d
olaifa ibukun <ibukunolaifa1984@example.com>
2024-02-06 14:26:08 +0100

    production

--- aimedic/settings/production_settings.py
+++ aimedic/settings/production_settings.py
@@ -7,6 +7,7 @@ from .base_settings import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_env_variable("SECRET_KEY", "XXXX")
+DEBUG = False
 
 ALLOWED_HOSTS.append("*")
 
