6978034e259969c6dc3c979772d247a3dc46d0a8
olaifa ibukun <ibukunolaifa1984@example.com>
2024-02-07 21:42:00 +0100

    testing cron job

--- home/views.py
+++ home/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
+from django.core.mail import send_mail
 
 
 def run_cronjob(request):
+    send_mail(
+        "Cron job",
+        "cron job ran",
+        recipient_list=[
+            "ibukunolaifa@gmail.com",
+            "ibukunolaifa1984@gmail.com"
+        ]
+    )
     print("running cronjob")
     return HttpResponse("Ok")
