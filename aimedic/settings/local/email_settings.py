905e6903cb6e52695eea6b9ef4476dd5681c37c4
olaifa ibukun <ibukunolaifa1984@example.com>
2024-02-08 13:25:46 +0100

    fixed mail sending error

--- aimedic/settings/local/email_settings.py
+++ aimedic/settings/local/email_settings.py
@@ -15,10 +15,8 @@ EMAIL_USE_TLS = True
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
 SERVER_EMAIL = EMAIL_HOST_USER
 
-DEFAULT_FROM_EMAIL = "noreply@afrimed.com" or EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 ADMINS = [(get_env_variable("ADMIN1", "XXX"), (get_env_variable("ADMIN2", "XXX")))]
