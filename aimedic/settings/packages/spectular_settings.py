99c7f4941cc9b67ad3b875099a52ee55cd64555e
olaifa ibukun <ibukunolaifa1984@example.com>
2024-02-18 12:50:45 +0100

    updated API title and description on swagger

--- aimedic/settings/packages/spectular_settings.py
+++ aimedic/settings/packages/spectular_settings.py
@@ -5,8 +5,8 @@ from aimedic.utils.choices import (
 
 SPECTACULAR_SETTINGS = {
     "SCHEMA_PATH_PREFIX": "/api",
-    "TITLE": "aimedic API",
-    "DESCRIPTION": "The AI Medic aims to revolutionize healthcare in Africa by addressing the severe shortage of Radiologists. Leveraging  AI innovative technology, this solution aids medical image analysis to enhance patient-doctor interactions and provide accurate diagnoses.",
+    "TITLE": "Afrimed API",
+    "DESCRIPTION": "The Afrimed aims to revolutionize healthcare in Africa by addressing the severe shortage of Radiologists. Leveraging  AI innovative technology, this solution aids medical image analysis to enhance patient-doctor interactions and provide accurate diagnoses.",
     "VERSION": "0.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
     "ENUM_NAME_OVERRIDES": {
